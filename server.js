844549f8f6b5212562f35551b8bca6aa4ff1c365
evdokimovm <tel89147925532@gmail.com>
2015-12-06 00:19:32 +1000

    POST and PUT requests

--- server.js
+++ server.js
@@ -15,6 +15,38 @@ router.get('/', function(req, res) {
 	res.json(json);
 });
 
+router.post('/', function(req, res) {
+	// insert the new item into the collection (validate first)
+	if (req.body.Id && req.body.Title && req.body.Director && req.body.Year && req.body.Rating) {
+		json.push(req.body);
+		res.json(json);
+	} else {
+		res.json(500, {
+			error: 'There was an error!'
+		});
+	}
+});
+
+router.put('/:id', function(req, res) {
+	// update the item in the collection
+	if (req.params.id && req.body.Title && req.body.Director && req.body.Year && req.body.Rating) {
+		_.each(json, function(elem, index) {
+			// find and update:
+			if (elem.Id === req.params.id) {
+				elem.Title = req.body.Title;
+				elem.Director = req.body.Director;
+				elem.Year = req.body.Year;
+				elem.Rating = req.body.Rating;
+			}
+		});
+		res.json(json);
+	} else {
+		res.json(500, {
+			error: 'There was an error!'
+		});
+	}
+});
+
 app.use('/', router);
 
 var server = app.listen(app.get('port'), function() {
